93f03a1728c765e159aab0e44d7a3c81a2366791
Owais Khan <oaak78692@gmail.com>
2024-03-02 12:21:01 +0530

    completed

--- weatherapp/src/InfoBox.jsx
+++ weatherapp/src/InfoBox.jsx
@@ -4,19 +4,38 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import './InfoBox.css';
 
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
+
 export default function InfoBox({info},) {
-    let imgURL="https://images.unsplash.com/photo-1680352267694-a7fd4c33d4e1?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    const imgURL="https://images.unsplash.com/photo-1680352267694-a7fd4c33d4e1?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    const hotURL="https://images.unsplash.com/photo-1577985759186-0854dfd3f218?q=80&w=1514&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    const coldURL="https://images.unsplash.com/photo-1612208695882-02f2322b7fee?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    const rainURL="https://images.unsplash.com/photo-1438449805896-28a666819a20?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
     return (
         <div className='cardContainer'>
         <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         sx={{ height: 140 }}
-        image={imgURL}
+        image={
+          info.humidity > 80
+          ? rainURL
+          : info.temp > 15
+          ? hotURL
+          : coldURL
+        }
         title="green iguana"
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-            {info.location}
+            {info.location}&nbsp;&nbsp;{
+          info.humidity > 80
+          ? <ThunderstormIcon/>
+          : info.temp > 15
+          ? <WbSunnyIcon/>
+          : <AcUnitIcon/>
+        }
         </Typography>
         <Typography variant="body2" color="text.secondary" component={"span"}>
             <p>Temp : {info.temp}&deg;C</p>
